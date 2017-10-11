c12bee84d7f88a51697edcc2387b4984f824bdde
Komn <kungfupanda316@outlook.com>
2017-10-11 12:51:41 +0800

    v1.0

---
+++ src/android/com/creative/informatics/camera/ResultActivity.java
@@ -0,0 +1,56 @@
+package com.creative.informatics.camera;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by K on 9/29/2017.
+ */
+
+public class ResultActivity extends Activity {
+
+    private Bitmap mResultBmp;
+    private Bitmap mFinalBmp;
+    private ImageView imgView;
+    private Button rotateButton;
+    private Button finishButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_view);
+
+        imgView = (ImageView)findViewById(R.id.resultview);
+        String path = getIntent().getStringExtra("resultpath");
+        mResultBmp = BitmapFactory.decodeFile(path);
+        imgView.setImageBitmap(mResultBmp);
+
+        rotateButton = (Button)findViewById(R.id.rotate);
+
+        rotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFinalBmp = RotateBitmap(mResultBmp,90);
+                imgView.setImageBitmap(mFinalBmp);
+                mResultBmp = mFinalBmp;
+            }
+        });
+
+    }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+
+}
