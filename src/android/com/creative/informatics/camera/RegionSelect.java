2503925328d5cb0c5c18129120c7308a4f2328e5
Komn <kungfupanda316@outlook.com>
2017-10-12 02:34:58 +0800

    v3.1

--- src/android/com/creative/informatics/camera/RegionSelect.java
+++ src/android/com/creative/informatics/camera/RegionSelect.java
@@ -42,10 +42,10 @@ public class RegionSelect extends View{
         super(context);
 
         mPaint = new Paint();
-        mPaint.setColor(getResources().getColor(getResources().getIdentifier("polygonViewCircleBackground", "color", getPackageName())));
+        mPaint.setColor(getResources().getColor(context.getResources().getIdentifier("polygonViewCircleBackground", "color", getPackageName())));
 
         nPaint = new Paint();
-        nPaint.setColor(getResources().getColor(getResources().getIdentifier("blue", "color", getPackageName())));
+        nPaint.setColor(getResources().getColor(context.getResources().getIdentifier("blue", "color", getPackageName())));
         nPaint.setStyle(Paint.Style.STROKE);
         nPaint.setStrokeWidth(5.0f);
 
