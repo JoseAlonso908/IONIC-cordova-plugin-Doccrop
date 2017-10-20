1b152f75f0984f06f4cab139ee21d5e437e2e249
Komn <kungfupanda316@outlook.com>
2017-10-20 17:37:43 +0800

    ljljl

--- src/android/com/creative/informatics/camera/DocDetect.java
+++ src/android/com/creative/informatics/camera/DocDetect.java
@@ -2,6 +2,7 @@ package com.creative.informatics.camera;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
