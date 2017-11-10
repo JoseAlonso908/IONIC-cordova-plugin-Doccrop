c4d72811b0e54b6a5d83faf8aaa6180c927b4f35
Komn <kungfupanda316@outlook.com>
2017-11-11 01:05:43 +0800

    firstopencv

---
+++ src/ios/ResultController.h
@@ -0,0 +1,9 @@
+
+#import <UIKit/UIKit.h>
+#import "CVWrapper.h"
+
+@interface ResultController : UIViewController
+
+@property(nonatomic,retain) UIImage* resultimg;
+
+@end
---
+++ src/ios/ViewController.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVCaptureSession.h>
+#import <AVFoundation/AVCaptureStillImageOutput.h>
+#import <AVFoundation/AVCaptureInput.h>
+#import <AVFoundation/AVCaptureVideoPreviewLayer.h>
+#import <AVFoundation/AVCaptureVideoDataOutput.h>
+
+@interface ViewController : UIViewController<UIImagePickerControllerDelegate>
+
+@end
+
