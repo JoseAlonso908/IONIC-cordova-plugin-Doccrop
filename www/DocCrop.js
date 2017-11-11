66bd03e35da124ec0f1212214738b6c82d34d860
Komn <kungfupanda316@outlook.com>
2017-11-11 23:43:59 +0800

    jkjfasdk

--- www/DocCrop.js
+++ www/DocCrop.js
@@ -3,7 +3,7 @@ var exec = require("cordova/exec");
 var DocCrop = function () {};
 
 DocCrop.prototype.cropresult = function (onSuccess, onFail) {
-  exec(onSuccess, onFail, "DocCrop", "result");
+  exec(onSuccess, onFail, "DocCrop", "cropresult");
 };
 
 module.exports = new DocCrop();
